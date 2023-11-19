4c59f9c715b27088da952f262e7753fef2ea6590
BerkeSaylan <berksay@yahoo.com>
2023-11-19 14:32:59 +0100

    bubbleSort updated, new examples added

--- ArrayExamples/ase/BubbleSortExample.java
+++ ArrayExamples/ase/BubbleSortExample.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 
 public class BubbleSortExample {
     public static void bubbleSort(int[] array) {
-        int n = array.length;
         // Traverse through all array elements
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             // Last i elements are already in place, so we don't need to check them
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < array.length - 1; j++) {
                 // Swap if the element found is greater than the next element
-                if (j < n - 1 && array[j] > array[j + 1]) {
+                if (array[j] > array[j + 1]) {
                     // Swap array[j] and array[j + 1]
                     int temp = array[j];
                     array[j] = array[j + 1];
@@ -30,7 +29,29 @@ public class BubbleSortExample {
         we only have consider the remaining 8, as the last index is already correct
          */
 
+
     public static void bubbleSort2(int[] array) {
+        // Traverse through all array elements
+        for (int i = 0; i < array.length - 1; i++) {
+            // Last i elements are already in place, so we don't need to check them
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                // Swap if the element found is greater than the next element
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    /* Another way to implement bubbleSort, only difference to version 2 is that after every iteration
+    the index in the front is assigned the correct value, e.g. after iteration 1 the smallest number
+    is in index 0
+     */
+    public static void bubbleSort3(int[] array) {
         // Traverse through all array elements
         for (int k = 0; k < array.length - 1; k++) {
             // Last k elements are already in place, so we don't need to check them
@@ -47,14 +68,14 @@ public class BubbleSortExample {
     }
 
 
-        public static void main(String[] args) {
-            int[] array = {64, 34, 25, 12, 22, 11, 90};
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90, 0};
 
-            System.out.println("Original array: " + Arrays.toString(array));
+        System.out.println("Original array: " + Arrays.toString(array));
 
-            bubbleSort(array);
+        bubbleSort3(array);
 
-            System.out.println("Sorted array: " + Arrays.toString(array));
-        }
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
 
 }
